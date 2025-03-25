5bcd4acfd50799564518f8beb3431691950cf856
Kechida98 <kechida.ok@gmail.com>
2025-03-25 10:43:05 +0100

    Setup motor pins and PWM for DC motor
    Configured Motor A control pins and attached a PWM channel to the enable pin. Serial monitor initialized for testing with a basic debug message.

--- main/sketch.cpp
+++ main/sketch.cpp
@@ -2,19 +2,29 @@
 Prototype one was focus on getting motor A to move forward and backward,
 check Readme.md file for Pictures.
 */
-
+#include <Arduino.h>
 // Motor A
 int motor1Pin1 = 21; 
 int motor1Pin2 = 20; 
 int enable1Pin = 22; 
 
-int motor2Pin3 = 2; 
-int motor2Pin4 = 3;     
-int enable2Pin = 4; 
-
 // Setting PWM properties
 const int freq = 30000;
 const int pwmChannel1 = 0;
-const int pwmChannel2 = 1;
 const int resolution = 8;
 int dutyCycle = 200;
+
+void setup() {
+  // sets the pins as outputs:
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+  
+  // configure LEDC PWM
+  ledcAttachChannel(enable1Pin, freq, resolution, pwmChannel1);
+
+  Serial.begin(115200);
+
+  // testing
+  Serial.print("Testing DC Motor...");
+}
