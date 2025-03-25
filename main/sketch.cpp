d16e1dcd1c7ffdda30fcd8fb3f1ef6e25cf75b0d
Kechida98 <kechida.ok@gmail.com>
2025-03-25 10:20:39 +0100

    Initial motor control setup for prototype 1
    Tested forward and backward movement of Motor A using PWM. See README.md for pictures.

--- main/sketch.cpp
+++ main/sketch.cpp
@@ -1,1 +1,20 @@
-hi again
+/*This code was first created in Arduino Ide.
+Prototype one was focus on getting motor A to move forward and backward,
+check Readme.md file for Pictures.
+*/
+
+// Motor A
+int motor1Pin1 = 21; 
+int motor1Pin2 = 20; 
+int enable1Pin = 22; 
+
+int motor2Pin3 = 2; 
+int motor2Pin4 = 3;     
+int enable2Pin = 4; 
+
+// Setting PWM properties
+const int freq = 30000;
+const int pwmChannel1 = 0;
+const int pwmChannel2 = 1;
+const int resolution = 8;
+int dutyCycle = 200;
