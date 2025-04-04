0a86b7f6923df31536a22d55289f2c6557aafc97
Kechida98 <kechida.ok@gmail.com>
2025-04-04 11:03:31 +0200

    Added controller connection callback
    This callback function is triggered when a new gamepad connects. It checks for an available slot in the myControllers array and stores the connected controller there. If no slots are available, a message is logged. Additionally, it logs details about the connected controller’s model and IDs.

--- main/sketch.cpp
+++ main/sketch.cpp
@@ -21,7 +21,31 @@ const int pwmChannel2 = 1;
 const int resolution = 8;
 int dutyCycle = 200;
 
+#include <Arduino.h>
+#include <Bluepad32.h>
 ControllerPtr myController [BP32_MAX_GAMEPADS];
+
+// This callback gets called any time a new gamepad is connected.
+// Up to 4 gamepads can be connected at the same time.
+void onConnectedController(ControllerPtr ctl) {
+    bool foundEmptySlot = false;
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == nullptr) {
+            Console.printf("CALLBACK: Controller is connected, index=%d\n", i);
+            // Additionally, you can get certain gamepad properties like:
+            // Model, VID, PID, BTAddr, flags, etc.
+            ControllerProperties properties = ctl->getProperties();
+            Console.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName(), properties.vendor_id,
+                           properties.product_id);
+            myControllers[i] = ctl;
+            foundEmptySlot = true;
+            break;
+        }
+    }
+    if (!foundEmptySlot) {
+        Console.println("CALLBACK: Controller connected, but could not found empty slot");
+    }
+}
 
 void setup() {
   // sets the pins as outputs:
