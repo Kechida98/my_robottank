553c2e28fc9d8ae0978ef0b45f4f3be2715a3548
Kechida98 <kechida.ok@gmail.com>
2025-04-07 13:56:21 +0200

    Removed hardcoded motor test sequence and setup debug prints
    Removed manual motor movement tests and initial serial prints to prepare for PS4 controller input control.

--- main/sketch.cpp
+++ main/sketch.cpp
@@ -136,6 +136,18 @@ void processControllers(){
 }
 
 void setup() {
+      // sets the pins as outputs:
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+
+  pinMode(motor2Pin3, OUTPUT);
+  pinMode(motor2Pin4, OUTPUT);
+  pinMode(enable2Pin, OUTPUT);
+  
+  // configure LEDC PWM
+  ledcAttachChannel(enable1Pin, freq, resolution, pwmChannel1);
+  ledcAttachChannel(enable2Pin, freq, resolution, pwmChannel2);
 
     Console.printf("Firmware: %s\n", BP32.firmwareVersion());
     const uint8_t* addr = BP32.localBdAddress();
@@ -151,24 +163,6 @@ void setup() {
     BP32.forgetBluetoothKeys();
      // Optional: enable BLE service (e.g. for apps like LightBlue) change to "true" if i want it activated
     BP32.enableBLEService(false);
-
-  // sets the pins as outputs:
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(enable1Pin, OUTPUT);
-
-  pinMode(motor2Pin3, OUTPUT);
-  pinMode(motor2Pin4, OUTPUT);
-  pinMode(enable2Pin, OUTPUT);
-  
-  // configure LEDC PWM
-  ledcAttachChannel(enable1Pin, freq, resolution, pwmChannel1);
-  ledcAttachChannel(enable2Pin, freq, resolution, pwmChannel2);
-
-  Serial.begin(115200);
-
-  // testing
-  Serial.print("Testing DC Motor...");
   
 }
 
@@ -182,102 +176,4 @@ void loop() {
     //change to vTaskDelay when am using realtimesteering
     //     vTaskDelay(1);
     delay(150);
-
-  // Move the DC motor forward at maximum speed
-  Serial.println("Moving Forward");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin3, LOW);
-  digitalWrite(motor2Pin4, HIGH);
-  ledcWrite(pwmChannel1, dutyCycle);
-  ledcWrite(pwmChannel2, dutyCycle);
-  delay(2000);
-
-  // Stop the DC motor
-  Serial.println("Motor stopped");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin3, LOW);
-  digitalWrite(motor2Pin4, LOW);
-  ledcWrite(pwmChannel1, 0);
-  ledcWrite(pwmChannel2, 0);
-  delay(1000);
-
-  //Turn Right (Motor A forward and B Stop)
-  Serial.println("Pivot Right");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin3, LOW);
-  digitalWrite(motor2Pin4, LOW);
-  ledcWrite(pwmChannel1, dutyCycle);
-  ledcWrite(pwmChannel2, 0);
-  delay(1000);
-
-  //Stop
-  Serial.println("Stop")
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin3, LOW);
-  digitalWrite(motor2Pin4, LOW);
-  ledcWrite(pwmChannel1, 0);
-  ledcWrite(pwmChannel2, 0);
-  delay(1000);
-
-  //Opposite
-  Serial.println("Pivot Left");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin3, LOW);
-  digitalWrite(motor2Pin4, HIGH);
-  ledcWrite(pwmChannel1, 0);
-  ledcWrite(pwmChannel2, dutyCycle);
-  delay(1000);
-
-  //Stop
-  Serial.println("Stop")
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin3, LOW);
-  digitalWrite(motor2Pin4, LOW);
-  ledcWrite(pwmChannel1, 0);
-  ledcWrite(pwmChannel2, 0);
-  delay(1000);
-
-  // Move DC motor backwards at maximum speed
-  Serial.println("Moving Backwards");
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin3, HIGH);
-  digitalWrite(motor2Pin4, LOW);
-
-  ledcWrite(pwmChannel1, dutyCycle);
-  ledcWrite(pwmChannel2, dutyCycle);
-  delay(2000);
-
-  // Stop the DC motor
-  Serial.println("Motor stopped");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin3, LOW);
-  digitalWrite(motor2Pin4, LOW);
-
-  ledcWrite(pwmChannel1, 0);
-  ledcWrite(pwmChannel2, 0);
-  delay(1000);
-
-
-  // Move DC motor forward with increasing speed
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin3, LOW);
-  digitalWrite(motor2Pin4, HIGH);
-  while (dutyCycle <= 255){
-    ledcWrite(enable1Pin, dutyCycle);
-    ledcWrite(enable2Pin, dutyCycle);   
-    Serial.print("Forward with duty cycle: ");
-    Serial.println(dutyCycle);
-    dutyCycle = dutyCycle + 5;
-    delay(500);
-  }
-  dutyCycle = 200;
 }
