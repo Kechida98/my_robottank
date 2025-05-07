882c642684e14ed4541b90e6f5bcffc65dd76e59
Kechida98 <kechida.ok@gmail.com>
2025-05-07 14:15:30 +0200

    Initialize NVS required for Wi-Fi functionality
    Before utilizing Wi-Fi, it's crucial to initialize the Non-Volatile Storage (NVS). This commit adds the initialization code, handling scenarios where the storage is full or a new version is detected.

--- main/main.c
+++ main/main.c
@@ -35,6 +35,15 @@ int app_main(void) {
 #endif
 #endif
 
+    //Initialize NVS
+    ESP_LOGI(TAG, "Initializing NVS...");
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
     // Configure BTstack for ESP32 VHCI Controller
     btstack_init();
 
