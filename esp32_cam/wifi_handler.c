6a109768d8fe07b4008c668f0bfe2f4d68de0034
Kechida98 <kechida.ok@gmail.com>
2025-05-17 18:58:55 +0200

    Add required includes and reconnect counter for Wi-Fi handler
    Added all necessary header files for Wi-Fi, logging, networking, and system functionality
    Declared a static global variable reconnect_counter to track reconnection attempts after disconnection

---
+++ esp32_cam/wifi_handler.c
@@ -0,0 +1,9 @@
+#include "wifi_handler.h"
+#include "printer_helper.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+static int reconnect_counter = 0;
