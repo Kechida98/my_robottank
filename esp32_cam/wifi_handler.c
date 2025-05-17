9544aa9e9e3c9082a1b56ff01c5b3d04462c8883
Kechida98 <kechida.ok@gmail.com>
2025-05-17 19:00:58 +0200

    Add wifi_event_handler to manage Wi-Fi connection events
    This function handles various Wi-Fi station events. It initiates a connection when the station starts, sets connection flags upon successful connection, and handles disconnections by retrying a limited number of times before giving up.

--- esp32_cam/wifi_handler.c
+++ esp32_cam/wifi_handler.c
@@ -7,3 +7,34 @@
 #include "lwip/sys.h"
 
 static int reconnect_counter = 0;
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    wifi_init_param_t *param = (wifi_init_param_t *)arg;
+    switch (event_id) {
+        case WIFI_EVENT_STA_START:
+            PRINTFC_WIFI_HANDLER("WiFi started, attempting to connect...");
+            esp_wifi_connect();
+            break;
+
+        case WIFI_EVENT_STA_CONNECTED:
+            PRINTFC_WIFI_HANDLER("WiFi connected");
+            reconnect_counter = 0; // Nollställ om anslutningen lyckades
+            xEventGroupSetBits(param->wifi_event_group, WIFI_CONNECTED_BIT);
+            break;
+
+        case WIFI_EVENT_STA_DISCONNECTED:
+            PRINTFC_WIFI_HANDLER("WiFi disconnected");
+            xEventGroupClearBits(param->wifi_event_group, WIFI_CONNECTED_BIT | WIFI_HAS_IP_BIT);
+            if (reconnect_counter < WIFI_RECONNECT_MAX_ATTEMPT) {
+                reconnect_counter++;
+                PRINTFC_WIFI_HANDLER("Reconnecting (%d/%d)...", reconnect_counter, WIFI_RECONNECT_MAX_ATTEMPT);
+                esp_wifi_connect();
+            } else {
+                PRINTFC_WIFI_HANDLER("Reached maximum reconnection attempts.");
+            }
+            break;
+
+        default:
+            break;
+    }
+}
